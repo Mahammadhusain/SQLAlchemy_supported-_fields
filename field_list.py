b733aaa931c2e4fe6304e780002c79d36c70a775
Mahammmadhusain Kadiwala <62128991+Mahammadhusain@users.noreply.github.com>
2023-07-12 11:38:47 +0530

    Create field_list.py

---
+++ field_list.py
@@ -0,0 +1,41 @@
+BigInteger - A type for bigger int integers.
+
+Boolean - A bool datatype.
+
+Date - A type for datetime.date() objects.
+
+DateTime - A type for datetime.datetime() objects.
+
+Double - A type for double FLOAT floating point types.
+
+Enum - Generic Enum Type.
+
+Float - Type representing floating point types, such as FLOAT or REAL.
+
+Integer - A type for int integers.
+
+Interval - A type for datetime.timedelta() objects.
+
+LargeBinary - A type for large binary byte data.
+
+MatchType - Refers to the return type of the MATCH operator.
+
+Numeric - Base for non-integer numeric types, such as NUMERIC, FLOAT, DECIMAL, and other variants.
+
+PickleType - Holds Python objects, which are serialized using pickle.
+
+SchemaType - Add capabilities to a type which allow for schema-level DDL to be associated with a type.
+
+SmallInteger - A type for smaller int integers.
+
+String - The base for all string and character types.
+
+Text - A variably sized string type.
+
+Time - A type for datetime.time() objects.
+
+Unicode - A variable length Unicode string type.
+
+UnicodeText - An unbounded-length Unicode string type.
+
+Uuid - Represent a database agnostic UUID datatype.
